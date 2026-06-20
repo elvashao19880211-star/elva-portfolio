070f532155a5ef13ef246edf321931bb07bb0f2f
Elva Shao <elvashao19880211-star@users.noreply.github.com>
2026-06-20 22:22:24 +0800

    fix: 修复gitignore编码+删除me路由+移除.next追踪

--- .next/server/chunks/ssr/_next-internal_server_app_gallery2_page_actions_0we.ua_.js
+++
@@ -1,3 +0,0 @@
-module.exports=[97860,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_gallery2_page_actions_0we.ua_.js.map
--- .next/server/chunks/ssr/_next-internal_server_app_patterns_page_actions_06zp4ir.js
+++
@@ -1,3 +0,0 @@
-module.exports=[14487,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_patterns_page_actions_06zp4ir.js.map
